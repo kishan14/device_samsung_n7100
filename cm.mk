01b0787ea6644357e3efcc12f7a07e5d5feccdf3
kishan14 <kishanj57@gmail.com>
2014-08-15 23:18:29 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -5,8 +5,8 @@ PRODUCT_RELEASE_NAME := n7100
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
-# Inherit common device configuration
-$(call inherit-product, vendor/cm/config/common.mk)
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/n7100/full_n7100.mk)
 # Device identifier. This must come after all inclusions
@@ -16,8 +16,4 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-N7100
 PRODUCT_MANUFACTURER := samsung
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += \
-PRODUCT_NAME=t03gxx \
-TARGET_DEVICE=t03g \
-PRIVATE_BUILD_DESC="t03gxx-user 4.3 JSS15J N7100XXUENB2 release-keys" \
-BUILD_FINGERPRINT="samsung/t03gxx/t03g:4.3/JSS15J/N7100XXUENB2:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t03gxx TARGET_DEVICE=t03g BUILD_FINGERPRINT="samsung/t03gxx/t03g:4.1.1/JRO03C/N7100XXALJ3:user/release-keys" PRIVATE_BUILD_DESC="t03gxx-user 4.1.1 JRO03C N7100XXALJ3 release-keys"
